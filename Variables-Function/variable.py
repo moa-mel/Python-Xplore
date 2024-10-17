39287f1aacd2fca35378aa798fcb6cbb5863f047
Olaitan <olaitanmaryakano@gmail.com>
2024-10-17 23:44:53 +0100

    variable study

---
+++ Variables-Function/variable.py
@@ -0,0 +1,10 @@
+player_choice = "rock"
+computer_choice = "paper"
+
+# Variables are memory location where values are stored. VALUES are assigned to a variable
+# Any data type in python such as integers, strings and list are valus to be assigned to a variable
+#  Two types of Variable: a) Local b) Global
+# a) Local Variable: Variable devlared within the python function. I exist within the function and can not be access else where
+# b) Global Variable: They are declared outside the function, at the start of the code. It can be accessed within the function
+# c) Instance: 
+# d) Class: creating a new type of object
